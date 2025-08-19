07e17fb009d4b06347b7a758c7e1fc4aa84b2527
Inki Hong <ihong3589@gmail.com>
2025-08-19 08:28:16 +0000

    string padding in place for printing data, need to get correct data for current and highest amounts for both coins and fiat currencies

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ while has_next:
     all_accounts.extend(accounts.accounts)
 
 # watchlist_accounts = rest_client.get_accounts().accounts
-watchlist_accounts = [a for a in all_accounts if a.currency in assets.keys()]
+watchlist_accounts = [a for a in all_accounts if a.currency in assets.keys() and a.name.endswith('Wallet')]
 filled_orders = rest_client.list_orders(order_status='FILLED', start_date='2025-01-01T00:00:00.000Z')
 
 
@@ -69,25 +69,45 @@ for order in reversed(filled_orders.orders):
             asset.current_fiat_amount = 0
             asset.current_coin_amount = record.coin_amount
         asset.net_profit = asset.current_fiat_amount - asset.invested_fiat_amount
-        # asset.current_fiat_amount = record.fiat_amount
-        # asset.current_coin_amount = record.coin_amount
-        # asset.latest_action = record.type
-    # asset = assets
-    # logging.info(order)
-    # logging.info("")
-    # o = Order.from_dict(order.to_dict())
-    # active_orders.append(o)
-    # logging.info(order)
-    # refer to order_sample.json for sample output
 
 # in here it will iterate through the assets and calculate the remaining values
-watchlist_assets = set(assets.keys())
+for account in watchlist_accounts:
+    coin_key = account.currency
+    if coin_key in assets:
+        asset = assets[coin_key]
+        asset.current_coin_amount = float(account.available_balance['value']) + float(account.hold['value'])
+        
+# for coin, asset in assets.items():
+#     account 
+#     if asset.current_fiat_amount == 0 and asset.current_coin_amount == 0:
+#         asset.current_fiat_amount = asset.invested_fiat_amount
+        # asset.current_coin_amount = asset.initial_coin_amount
+
+# "available_balance": {
+#                 "value": "0",
+#                 "currency": "CBETH"
+#             },
+
+# "hold": {
+#                 "value": "0",
+#                 "currency": "CBETH"
+#             },
+
 
 print("Coin | Invested Amount | Initial Coin Amount | Current USD Amount | Highest USD Amount | Current Coin Amount | Highest Coin Amount | Net Profit | Latest Action")
 
 
 for asset in assets.values():
-    print(f"{asset.coin.value}  | {asset.invested_fiat_amount:.2f} | {asset.initial_coin_amount:.8f} | {asset.current_fiat_amount:.2f} | {asset.highest_fiat_amount:.2f} | {asset.current_coin_amount:.8f} | {asset.highest_coin_amount:.8f} | {asset.net_profit:.2f} | {asset.latest_action.value}")
+    col2 = f"{asset.invested_fiat_amount:.2f}".ljust(15, ' ')
+    col3 = f"{asset.initial_coin_amount:.8f}".ljust(19, ' ')
+    col4 = f"{asset.current_fiat_amount:.2f}".ljust(18, ' ')
+    col5 = f"{asset.highest_fiat_amount:.2f}".ljust(18, ' ')
+    col6 = f"{asset.current_coin_amount:.8f}".ljust(19, ' ')
+    col7 = f"{asset.highest_coin_amount:.8f}".ljust(19, ' ')
+    col8 = f"{asset.net_profit:.2f}".ljust(10, ' ')
+    col9 = asset.latest_action.value.ljust(12, ' ')
+    # print(f"{asset.coin.value}  | {asset.invested_fiat_amount:.2f} | {asset.initial_coin_amount:.8f} | {asset.current_fiat_amount:.2f} | {asset.highest_fiat_amount:.2f} | {asset.current_coin_amount:.8f} | {asset.highest_coin_amount:.8f} | {asset.net_profit:.2f} | {asset.latest_action.value}")
+    print(f"{asset.coin.value}  | {col2} | {col3} | {col4} | {col5} | {col6} | {col7} | {col8} | {col9}")
 
 # for account in non_empty_accounts:
     # logging.info(account)
