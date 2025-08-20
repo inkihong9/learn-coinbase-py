bd3a4e7f2bb43aead575f9f4422b8edae489e0a9
Inki Hong <ihong3589@gmail.com>
2025-08-20 21:23:18 +0000

    retrieve the watchlist products and calculate the current fiat amount

--- main.py
+++ main.py
@@ -41,24 +41,26 @@ if not coinbase_api_key or not coinbase_api_secret:
 
 rest_client = RESTClient(coinbase_api_key, coinbase_api_secret)
 
-all_accounts = []
+cb_all_accounts = []
 has_next = True
 cursor = None
 
 while has_next:
-    accounts = rest_client.get_accounts(cursor=cursor)
-    has_next = accounts.has_next
+    cb_accounts = rest_client.get_accounts(cursor=cursor)
+    has_next = cb_accounts.has_next
     if has_next:
-        cursor = accounts.cursor
+        cursor = cb_accounts.cursor
     
-    all_accounts.extend(accounts.accounts)
+    cb_all_accounts.extend(cb_accounts.accounts)
 
 # watchlist_accounts = rest_client.get_accounts().accounts
-watchlist_accounts = [a for a in all_accounts if a.currency in assets.keys() and a.name.endswith('Wallet')]
-filled_orders = rest_client.list_orders(order_status='FILLED', start_date='2025-01-01T00:00:00.000Z')
+cb_watchlist_accounts = [a for a in cb_all_accounts if a.currency in assets.keys() and a.name.endswith('Wallet')]
+cb_filled_orders = rest_client.list_orders(order_status='FILLED', start_date='2025-01-01T00:00:00.000Z')
+cb_products = rest_client.get_products(product_ids=['XRP-USD', 'ETH-USD', 'ADA-USD', 'DOT-USD', 'CRO-USD', 'SOL-USD'])
+cb_products_dict = {p.product_id.split('-')[0]: p for p in cb_products.products}
 
 
-for order in reversed(filled_orders.orders):
+for order in reversed(cb_filled_orders.orders):
     record = Record.from_cb_order(order)
     coin_key = record.coin.value
     if coin_key in assets:
@@ -75,16 +77,21 @@ for order in reversed(filled_orders.orders):
         asset.net_profit = asset.current_fiat_amount - asset.invested_fiat_amount
 
 # iterate through the watchlist accounts and calculate the remaining values
-for account in watchlist_accounts:
-    coin_key = account.currency
+for cb_account in cb_watchlist_accounts:
+    coin_key = cb_account.currency
     if coin_key in assets:
+        cb_product = cb_products_dict.get(coin_key)
         asset = assets[coin_key]
-        asset.current_coin_amount = float(account.available_balance['value']) + float(account.hold['value'])
+        asset.current_coin_amount = float(cb_account.available_balance['value']) + float(cb_account.hold['value'])
+        asset.current_fiat_amount = asset.current_coin_amount * float(cb_product.price)
+
+# bid price = highest price buyers are willing to pay
+# ask price = lowest price sellers are willing to accept
+# spread = ask price - bid price
 
 
+# print the dashboard
 print("Coin | Invested Amount | Initial Coin Amount | Current USD Amount | Highest USD Amount | Current Coin Amount | Highest Coin Amount | Net Profit | Latest Action")
-
-
 for asset in assets.values():
     col2 = f"{asset.invested_fiat_amount:.2f}".ljust(15, ' ')
     col3 = f"{asset.initial_coin_amount:.8f}".ljust(19, ' ')
